bcfb24fe6365418b5a2d061c515c060a61d031e9
ermi637 <erumimran260@gmail.com>
2025-06-28 17:38:47 +0500

    Update layout.tsx
    added icons: {
        icon: '/favicon.ico', // Make sure this file is in the "public" folder
      },

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: 'Real Estate | Trusted Property Dealer in Karachi',
   description: 'Buy, sell, or rent properties in Karachi with our expert real estate services. Trusted by thousands',
   generator: 'Real Estate by Ch Talha',
+   icons: {
+    icon: '/download.png', // Make sure this file is in the "public" folder
+  },
 }
 
 export default function RootLayout({
